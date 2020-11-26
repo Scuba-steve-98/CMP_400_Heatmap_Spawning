2061c75d9f8c4457532ccaa9de7081f569d5b36e
Scuba-steve-98 <47321591+Scuba-steve-98@users.noreply.github.com>
2020-11-26 17:31:50 +0000

    Heatmap works at any scale

--- CMP 400 Heatmap Spawning/Assets/HeatmapSetup.cs
+++ CMP 400 Heatmap Spawning/Assets/HeatmapSetup.cs
@@ -8,36 +8,34 @@ public class HeatmapSetup : MonoBehaviour
 
     float widthTemp, bredthTemp, distance, colliderRadius;
 
-    bool isNear;
-
     Vector3 planeSize, bottomLeft, rayStart, rayEnd, defaultVec;
-
-    Color32 defaultColour, peakThreat;
 
     float[,] threatLevel;
 
     Tiles[] tiles;
 
     [SerializeField]
-    [Range(0, 1)]
-    int scale = 3;
+    [Range(1, 10)]
+    int scale = 5;
 
     // Start is called before the first frame update
     void Start()
     {
         index = 0;
-        isNear = false;
         distance = 0f;
-        defaultVec = new Vector3(1, 1, 1);
+        defaultVec = new Vector3(1, 1, 1) * scale;
+        defaultVec.y = 1;
         planeSize = GetComponent<MeshRenderer>().bounds.size;
-        width = (int)planeSize.x;// * scale;
-        bredth = (int)planeSize.z;// * scale;
+        width = (int)planeSize.x / scale;
+        bredth = (int)planeSize.z / scale;
+        Debug.Log("Plane Size: " + planeSize.x + ", Width: " + width);
         bottomLeft = gameObject.transform.position - (planeSize / 2);
         layerMask = 1 << 9;
-        defaultColour = new Color32(0, 0, 0, 1);
-        peakThreat = new Color32(255, 0, 0, 1);
-        threatLevel = new float[20, 20];
-        tiles = new Tiles[400];
+        int platWidth = (int)transform.localScale.x * 5;
+        int platBredth = (int)transform.localScale.z * 5;
+        threatLevel = new float[platWidth, platBredth];
+        int arrayIndexMax = platBredth * platWidth;
+        tiles = new Tiles[arrayIndexMax];
     }
 
     // Update is called once per frame
@@ -48,13 +46,13 @@ public class HeatmapSetup : MonoBehaviour
         {
             for (int j = 0; j < bredth; j++)
             {
-                widthTemp = (float)i + 0.5f;// / scale;
-                bredthTemp = (float)j + 0.5f;// / scale;
+                widthTemp = (float)i * scale + (0.5f * scale);
+                bredthTemp = (float)j * scale + 0.5f * scale;
                 rayStart = bottomLeft + Vector3.right * widthTemp + Vector3.forward * bredthTemp;
                 rayEnd = rayStart + Vector3.up * 5;
                 RaycastHit[] hit;
                 hit = Physics.RaycastAll(rayStart, rayEnd, 5, layerMask);
-                //Debug.DrawLine(rayStart, rayEnd);
+                Debug.DrawLine(rayStart, rayEnd);
                 //threatLevel[i, j] = rayStart;
                 threatLevel[i, j] = 0f;
                 
@@ -65,9 +63,9 @@ public class HeatmapSetup : MonoBehaviour
                         float threat = hit[c].collider.gameObject.GetComponentInParent<ThreatData>().getThreatLevel();
                         colliderRadius = hit[c].collider.transform.localScale.x;
                         distance = Vector3.Distance(rayStart, hit[c].transform.position);
-                        Debug.Log("Distance: " + distance);
+                        //Debug.Log("Distance: " + distance);
                         threatLevel[i, j] += Mathf.Lerp(threat, 0, distance / (colliderRadius / 2f));
-                        Debug.Log(threatLevel[i, j]);
+                        //Debug.Log(threatLevel[i, j]);
                     }
                 }
                 tiles[index] = new Tiles(threatLevel[i, j], rayStart);
